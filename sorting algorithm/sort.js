f831f129b0f2a759f93c847eea4ec470b370ea3a
3dw1nM0535 <workockmoses@gmail.com>
2017-11-07 11:59:04 +0300

    defined selectionSort()

--- sorting algorithm/sort.js
+++ sorting algorithm/sort.js
@@ -15,6 +15,7 @@ function Carray (numElements) {
   this.setData = setData;
   this.swap = swap;
   this.bubbleSort = bubbleSort;
+  this.selectionSort = selectionSort;
 
   for (var i = 0; i < numElements; i++) {
     this.dataStore[i] = i;
@@ -62,7 +63,7 @@ function swap (arr, index1, index2) {
 }
 
 //class test data
-var numElements = 10;
+var numElements = 100;
 var nums = new Carray(numElements);
 nums.setData();
 console.log(nums.toString());
@@ -82,10 +83,44 @@ function bubbleSort() {
         swap(this.dataStore, inner, inner + 1);
       }
     }
-    console.log(this.toString());
+    //console.log(this.toString());
   }
 
 }
 
-nums.bubbleSort();
+//nums.bubbleSort();
+//console.log(nums.toString());
+
+//Selection sort algorithm
+//Selection sort algorithm start at the beginning of
+//the array and compare the first element
+//with the remaining elements in the array
+//After examining all the elements the smallest element is place at the first position
+//of the array
+//This continues until all the algorthim arrives at the next to last position
+//of the array at which all the elements are sorted
+
+//Nested loops are used in selection sort algorithm. The outer loop move from
+//first element to the next element to last element while the inner loop
+//moves from the second elementof the array looking for values that are
+//smaller than the element currently pointed out by the outer loop
+//After each iteration the smallest value is assigned its proper place in the array
+
+//selectionSort() function definition
+function selectionSort() {
+  //variable to hold minValue and temp
+  var minValue, temp;
+  for (var outer = 0; outer <= this.dataStore.length - 2; outer++) {
+    minValue = outer;
+    for (var inner = outer + 1; inner <= this.dataStore.length - 1; inner++) {
+      if (this.dataStore[inner] < this.dataStore[minValue]) {
+        minValue = inner;
+      }
+    }
+    swap(this.dataStore, outer, minValue);
+    //console.log(this.toString());
+  }
+}
+
+nums.selectionSort();
 console.log(nums.toString());
