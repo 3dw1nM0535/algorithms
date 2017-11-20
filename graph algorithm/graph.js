cc727601dfd6a3466fd30ea1d3d1b273eec36bb9
3dw1nM0535 <workockmoses@gmail.com>
2017-11-21 00:45:00 +0300

    graph algorithims definition

---
+++ graph algorithm/graph.js
@@ -0,0 +1,10 @@
+//Graph algorithm consist of vertices and edges.
+/**
+ * Vertex is a point in a graph and edge is a path connecting the vertex
+ * Edges are defined as a pair(v1, v2) which are two vertices in a graph
+ * Vertex can have a weight known as cost
+ * Graph whose pair is ordered is known as directed or digraph graph
+ * An example is a flowchart which shows computations
+ * of a computer program
+ * A graph which is not ordered is known as an unordered graph or just a graph
+ */
