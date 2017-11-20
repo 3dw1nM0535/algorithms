05b7c97a01a416bf166f70cdc3a81c9691a0a7eb
3dw1nM0535 <workockmoses@gmail.com>
2017-11-21 00:54:55 +0300

    occurence using object literals

--- test.js
+++ test.js
@@ -20,8 +20,19 @@ for (var i = 0; i < votes.length; i++) {
   }
 }
 
+//console.log(occurence);
+
+var properties = Object.keys(occurence).map(function (prop) {
+  return occurence[prop];
+});
+
+console.log(properties.join(' '));
+console.log(Object.keys(occurence).join(' '));
+
+/*
 for (var key in occurence) {
   if(occurence.hasOwnProperty(key)) {
     console.log(key + ':' + occurence[key]);
   }
 }
+*/
