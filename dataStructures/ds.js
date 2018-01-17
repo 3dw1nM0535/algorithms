1a67155c66ae30c0b2f65f5b54957eb0387b1d1c
3dw1nM0535 <workockmoses@gmail.com>
2018-01-17 10:57:18 +0300

    formatted doc blocks

--- dataStructures/ds.js
+++ dataStructures/ds.js
@@ -20,19 +20,23 @@ var numbers = []; //We just initialized an empty array variable called numbers
 console.log(numbers); // ==> displays []
 
 //You can also create an array by decalring an array of elements inside the array variable
+
 var numbers = [1, 2, 3, 4, 5, 6];
 console.log(numbers); // ==> displays [1,2,3,4,5,6]
 
 //You can also create an array by calling the Array constructor
+
 var numbers = new Array();
 console.log(numbers); // ==> displays []
 
 //You can also create an array by declaring an array of elemenets inside the Array constructor
+
 var numbers = new Array(1, 2, 3, 4, 5, 6);
 console.log(numbers); // ==> displays [1,2,3,4,5,6]
 
 //Also something of interest, JavaScript array does not need to be of same data type
 //Array can consist of Number String Boolean
+
 var objects = [null, 'Joe', 500, true];
 console.log(objects); // ==> displays [ null, 'Joe', 500, true ]
 
@@ -43,6 +47,7 @@ console.log(objects); // ==> displays [ null, 'Joe', 500, true ]
   */
 
   //We can also verify if an object or variable is an array by calling Array.isArray()
+  
 var number = 3;
 var numbers = [null, 'Joe', 500, true];
 console.log(Array.isArray(number)); //==> displays false
@@ -54,6 +59,7 @@ console.log(Array.isArray(numbers)); //==> displays true
    */
 //Data is assigned to array elements using the [] operator in an assignment statement 
 //For example the following loop assigns value 1 to 50 to an array
+
 var numbers = [];
 for (var i = 0; i < 50; i++) {
   numbers[i] = i + 1;
@@ -61,6 +67,7 @@ for (var i = 0; i < 50; i++) {
 console.log(numbers); //==> displays [1,2,3,4,5,6,7,........, 49, 50]
 
 //array elements can also be accessed using the [] operator
+
 var numbers = [1, 2, 3, 4];
 console.log(numbers[0] + ' ' + numbers[1] + ' ' + numbers[2] + ' ' + numbers[3]); //==> display 1 2 3 4
 
@@ -70,16 +77,20 @@ console.log(numbers[0] + ' ' + numbers[1] + ' ' + numbers[2] + ' ' + numbers[3])
     */
 
 //Ofcourse accessing all array elements sequentially is easier than before
+
 var number = [];
 for (var i = 0; i < 20; i++) {
   numbers[i] = i + 1;
 }
+
 //Use for loop to loop through the array and print all the lements in the array
+
 for (var i = 0; i < numbers.length; i++) {
   console.log(numbers[i]); //==> prints each element on its own line
 }
 
 //You can also do a running sum in the array
+
 var nums = [1, 3, 5, 7, 9, 4, 6, 90];
 var sum = 0;
 for (var i = 0; i < nums.length; i++) {
@@ -100,6 +111,7 @@ console.log(sum);
 */
 
 //The following program demos how split works on simple string word
+
 var sentence = 'You got balls providing CS tutorials without a CS degree';
 var words = sentence.split(' ');
 for (var i = 0; i < words.length; i++) {
@@ -135,20 +147,24 @@ for (var i = 0; i < words.length; i++) {
   */
 
  //Create array names array1
+ 
 var array1 = [];
 for (var i = 0; i < 10; i++) {
   array1[i] = i + 1;
 }
 
 //Copy array1 array elements to array2
+
 var array2 = array1;
 console.log(array2); //==> displays [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 //Make change to the original array
+
 array1[0] = 50;
 
 //Display array2. You'll notice 50 is on first index of the array as we
 //predicted
+
 console.log(array2);
 
 /**
@@ -162,6 +178,7 @@ console.log(array2);
   */
 
 //Define function to perform deep copy
+
 function deepCopy(arr1, arr2) {
   for (var i = 0; i < arr1.length; i++) {
     arr2[i] = arr1[i];
@@ -181,6 +198,7 @@ console.log(arr2); //==> displays [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1
 /**
  * SEARCH FOR VALUE IN AN ARRAY
  */
+
 function searchArray(arr, itemToSearch) {
   var position = arr.indexOf(itemToSearch);
   if (position > 0) {
@@ -193,6 +211,7 @@ function searchArray(arr, itemToSearch) {
 var names = ["David", "Cynthia", "Raymond", "Clayton", "Jennifer"];
 
 //Search test
+
 searchArray(names, 'Cynthia');
 
 /**
