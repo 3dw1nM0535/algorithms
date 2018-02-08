3266dfa97e17faded30ac40365280565002e7519
3dw1nM0535 <workockmoses@gmail.com>
2018-02-08 13:18:21 +0300

    defined Graph class with test data

--- graph algorithm/graph.js
+++ graph algorithm/graph.js
@@ -1,16 +1,15 @@
               /* Graph algorithms */
-              ======================
 /**
  * Graph algorithm consist of vertices and edges.
- * Vertex is a point in a graph and edges is a path connecting the vertex
- * edges are defined as a pair(v1, v2) which are two vertices in a graph
+ * Vertex is a point in a graph and edges is a path connecting the vertex.
+ * Edges are defined as a pair(v1, v2) which are two vertices in a graph
  * Vertex can have a weight known as cost
  * Graph whose pair is ordered is known as directed or digraph graph
  * An example is a flowchart which shows computations
  * of a computer program
  * A graph which is not ordered is known as an unordered graph or just a graph
  *
- * 
+ *
  * A path is a sequence of vertices in a graph such that all vertices in the path
  * are connected by edges. Length of a path is the number of edges from the first vertex
  * in the path to the last vertex. A path can also consist of a vertex to itself, which is
@@ -36,7 +35,7 @@
  *
  * Airline
  *
- * Airline company can use graph to model its flight system. Each airport is a vertex, and each flight
+ * Airline company can use graph to model its flight system. Each airport is a vertex, and each flight from
  * one vertex to another is an edge. Weighted edge can be used to represent the cost of the flight from
  * one airport to another, or the distance from one airport to another, depending on what is being modeled.
  *
@@ -45,7 +44,80 @@
  * Computer Netorks such as Local Area Network and much broader networks such as the Internet are modeled by
  * graghs.
  *
- * Anothe example of a real-world systems that can be modeled by graph is the consumer market, where vertices
+ * Another example of a real-world systems that can be modeled by graph is the consumer market, where vertices
  * represent both institutions(vendors) and consumers.
- *
+
+                    BUILDING A GRAPH CLASS
+
+ * The first step to building a Graph class is to build a Vertex class to store vertices of a graph. Vertex
+ * takes two parameters: one to identify the vertex and the other to store a Boolean value indicating whether
+ * or not the vertex has been visited.
  */
+
+ function Vertex (label) {
+   this.label = label;
+ }
+
+ // We will store the vertices in an array and reference then later in the Graph class by their position in the
+ // array.
+
+ /**
+  * The Real information about a graph is stored in the edges, since the edges describe the structures of a graph.
+  * With edges, their can be many edges linked to a single vertex or just one edge.
+
+  * The method we will use in representing edges of a graph is called adjacency list or array of adjacency lists.
+  * With this method, the edges are stored as a vertex-indexed array of lists(arrays) of the vertices adjacent to
+  * each other. Using this scheme, when we reference a vertex in a program, we can efficiently access the
+  * list of all the vertices it is connected to.
+
+  * Another method for representing the edges of a graph is called adjacency matrix. This is a two dimensional-array
+  * in which the elements of the array indicate whether an edge exist between two vertices.
+  */
+
+  function Graph (v) {
+    this.vertices = v;
+    this.edges = 0; // Keeps track of how many edges are represented in the Graph
+    this.adj = [];
+    for (var i = 0; i < this.vertices; i++) { // In each element of the array, loop adds subarray to store
+      this.adj[i] = []; // all the adjacent vertices
+      this.adj[i].push(''); // and initializes each element to the empty string
+    }
+    this.addEdge = addEdge;
+    this.showGraph = showGraph;
+  }
+
+  // addEdge() method definition.
+  // When called with two vertices(A, B), finds adjacency list for vertex A and adds B to the list, then finds
+  // adjacency list for vertex B and adds A to the list. Finally the function increments the number of edges by 1.
+
+  function addEdge (v, w) {
+    this.adj[v].push(w);
+    this.adj[w].push(v);
+    this.edges++;
+  }
+
+  // showGraph() method definition.
+  // displays a Graph by showing a list of all vertices and the vertices that are adjacent to them.
+
+  function showGraph () {
+    for (var i = 0; i < this.vertices; i++) {
+      console.log(i + ' => ');
+      for (var j = 0; j < this.vertices; j++) {
+        if (this.adj[i][j] != undefined) {
+          console.log(this.adj[i][j] + ' ');
+        }
+      }
+      console.log();
+    }
+  }
+
+  /**
+   * TEST PROGRAM FOR GRAPH CLASS
+   */
+
+  var g = new Graph(5);
+  g.addEdge(0,1);
+  g.addEdge(0,2);
+  g.addEdge(1,3);
+  g.addEdge(2,4);
+  g.showGraph();
