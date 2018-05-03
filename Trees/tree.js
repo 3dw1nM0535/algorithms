ccd7efa2b4def71fabe521b8d187a5b110580bbf
Edwin Moses <workockmoses@gmail.com>
2018-05-03 15:45:26 +0300

    defined insert() method for adding new node/key in the tree

--- Trees/tree.js
+++ Trees/tree.js
@@ -28,4 +28,74 @@
  * great-grandchildren(great-grandchild), and so on.
  *
  * Depth of a node consists of the number of ancestors.
+ *
+ *
+ * BINARY AND BINARY SEARCH TREES
+ *
+ * A node in a binary tree has two children at most: one left child and right child.
+ * This definition allows us to write more efficient algorithms to insert, search,
+ * and delete nodes to/from a binary tree. Binary trees are largely used in Computer
+ * Science.
+ *
+ * A binary search tree is a binary tree, but it allow us to only store lesser values
+ * on the left-hand side and nodes with greater values on the right-hand side.
  */
+
+function BST() {
+	let Node = function(key) {
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	};
+
+	// Helper for inserting new node
+	let insertNode = function(node, newNode) {
+		if (newNode.key < node.key) {
+			if (node.left === null) {
+				node.left = newNode;
+			} else {
+				insertNode(node.left, newNode);
+			}
+		} else {
+			if (node.right === null) {
+				node.right = newNode;
+			} else {
+				insertNode(node.right, newNode);
+			}
+		}
+	};
+
+	let root = null;
+
+	// Method insert() adds a new key/node in the tree
+	this.insert = function(key) {
+		let newNode = new Node(key);
+
+		if (root === null) {
+			root = newNode;
+		} else {
+			insertNode(root, newNode);
+		}
+	};
+}
+
+/**
+ * TREE TEST CASE
+ */
+
+let tree = new BST();
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(9);
+tree.insert(8);
+tree.insert(10);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
+tree.insert(20);
+tree.insert(18);
+tree.insert(25);
+tree.insert(6);
