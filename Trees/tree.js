3aebdb7154c7e2a6b545eee74eb54757bd102f5b
Edwin Moses <workockmoses@gmail.com>
2018-05-03 14:53:01 +0300

    tree concepts and definition of various tree terminologies

---
+++ Trees/tree.js
@@ -0,0 +1,31 @@
+/**
+ * Tree is a non-sequential data structure which is useful for storing
+ * information that needs to be found easily.
+ *
+ * We will cover:
+ * 	-Tree terminology
+ * 	-Create tree data structure
+ * 	-Traversing a tree
+ * 	-Adding or removing nodes
+ * 	-AVL tree
+ *
+ * Tree is an abstract model of a hierarchical structure. The most common
+ * example of a tree in real life is a family tree or a company or
+ * organizational chart.
+ *
+ * TREE TERMINOLOGY
+ *
+ * A tree consist of node with a parent-child relationship. Each node has a
+ * parent(except for the root node at the top) and zero or more children.
+ * Each element in the tree is a node. There are INTERNAL NODES and EXTERNAL NODES.
+ * An Internal node is a node with atleast one or more children.
+ * A node which has no children is called an External node.
+ *
+ * A node can have ancestors and descendants. Ancestors of a node(except the root node)
+ * are parents, grandparents, great-grandparents, and so on.
+ *
+ * The descendants of a node are children(child), grandchildren(grandchild),
+ * great-grandchildren(great-grandchild), and so on.
+ *
+ * Depth of a node consists of the number of ancestors.
+ */
